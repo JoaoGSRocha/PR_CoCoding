876f6b8de38cdaba28f52932843df2242a8bedf3
JoaoGSRocha <joaogsrocha@gmail.com>
2017-05-03 12:25:05 +0100

    Added servlet DatabseAccess and trying to make it compile

---
+++ src/main/resources/DatabaseAccess.java
@@ -0,0 +1,21 @@
+// Loading required libraries
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@SuppressWarnings("serial")
+public class DatabaseAccess extends HttpServlet{
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlet Get</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
