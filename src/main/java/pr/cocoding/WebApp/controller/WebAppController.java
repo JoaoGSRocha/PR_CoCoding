7a55fcf499492676c34163d18c9ad917cb533e95
pawc <pswid89@gmail.com>
2017-05-04 19:48:50 +0200

    bean configuration sample

--- src/main/java/pr/cocoding/WebApp/controller/WebAppController.java
+++ src/main/java/pr/cocoding/WebApp/controller/WebAppController.java
@@ -1,9 +1,14 @@
 package pr.cocoding.WebApp.controller;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import pr.cocoding.WebApp.model.Message;
+
 import org.springframework.ui.ModelMap;
 
 @Controller
@@ -11,8 +16,10 @@ public class WebAppController{
  
     @RequestMapping("/hello")
     public ModelAndView printHello(ModelMap model) {
-	    String message = "Spring MVC!";
-        return new ModelAndView("helloPage", "message", message);
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	    Message message = (Message) context.getBean("message");
+    	String content = message.getContent();
+        return new ModelAndView("helloPage", "message", content);
     }
 }
    
---
+++ src/main/java/pr/cocoding/WebApp/model/Message.java
@@ -0,0 +1,15 @@
+package pr.cocoding.WebApp.model;
+
+public class Message {
+
+	private String content;
+	
+	public void setContent(String content){
+		this.content = content;
+	}
+	
+	public String getContent(){
+		return content;
+	}
+	
+}
